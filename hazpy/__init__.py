3364e26d27bc90ade23383245e97be3ee3028da0
James Raines <jraines521@gmail.com>
2021-01-14 14:10:48 -0600

    Update __init__.py

--- hazpy/__init__.py
+++ hazpy/__init__.py
@@ -11,7 +11,8 @@
     :contributors: Ujvala K Sharma; ujvalak_in@yahoo.com,usharma@niyamit.com
     
 """
-__version__ = '0.0.2.dev4' # updated to match conda -c nhrap-test hazpy
+# updated to match conda -c nhrap-test hazpy
+__version__ = '0.0.2.dev4'
 __all__ = ['Tornado', 'Earthquake', 'Hurricane',
            'Tsunami', 'Flood', 'legacy', 'common', 'admin']
 
